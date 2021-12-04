7d87a122916cf211eb7dbd754a76d297a52a5d37
Jesse Falconer <jesse.falconer@spark.re>
2021-12-04 13:57:41 -0800

    Update to ruby 2.7.3

--- day1/1b.rb
+++ day1/1b.rb
@@ -1,3 +1,3 @@
 measurements = File.foreach("day1/input.txt").map(&:to_i).each_cons(3).map(&:sum)
 
-puts measurements.each_cons(2).count { |x| x[1] > x[0] }
+puts measurements.each_cons(2).count { _2 > _1 }
