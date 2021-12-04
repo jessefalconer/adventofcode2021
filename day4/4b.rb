ef14052004539196406d11f436e7f2d6fb2ae4be
Jesse Falconer <jesse.falconer@spark.re>
2021-12-04 13:22:32 -0800

    Refactors

--- day4/4b.rb
+++ day4/4b.rb
@@ -2,20 +2,19 @@ draws = File.read("day4/input.txt").split[0].split(",")
 boards = File.read("day4/input.txt").split(/\n{2,}/)[1..].map(&:split)
 queue = {}
 
+def board_won?(board, draw)
+  arr = board.each_slice(5).to_a
+
+  (arr + arr.transpose).any? { |row| row.all?(nil) }
+end
+
 draws.each do |draw|
   boards.each_with_index do |board, index|
     next if queue.keys.include? index
 
-    while (position = board.index(draw)) do
+    if (position = board.index(draw))
       board[position] = nil
-    end
-
-    arr = board.each_slice(5).to_a
-
-    (arr + arr.transpose).each do |row|
-      if row.all?(nil)
-        break queue[index] = board.flat_map(&:to_i).sum * draw.to_i
-      end
+      queue[index] = board.map(&:to_i).sum * draw.to_i if board_won?(board, draw)
     end
   end
 end
