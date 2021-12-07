2a7920d9ffc4923d35943107d88b7636c7751b67
Jesse Falconer <jesse.falconer@spark.re>
2021-12-07 09:47:06 -0800

    Use median linear regression for part 1 for fun

--- day7/7a.rb
+++ day7/7a.rb
@@ -1,7 +1,9 @@
 crabs = File.read("day7/input.txt").split(",").map(&:to_i)
 
 def optimize(crabs)
-  [*0..crabs.max].map { |x0| crabs.map{ (_1 - x0).abs }.sum }.min
+  y0 = (crabs.sort[(crabs.size - 1) / 2] + crabs.sort[crabs.size / 2]) / 2
+
+  crabs.map { (_1 - y0).abs }.sum
 end
 
 puts optimize(crabs)
